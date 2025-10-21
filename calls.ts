01795673dc084c64779c3736ec8adb2fe0305ba3
AKT <80599739+aktmath@users.noreply.github.com>
2025-10-21 20:26:57 +0300

    Update calls.ts

--- calls.ts
+++ calls.ts
@@ -1,4 +1,4 @@
-const counterContractAddress = '0xCD01BEc13cc523327F35599f19ff8A5d8f3041f4'; 
+const counterContractAddress = '0xCD01BEc13cc523327F35599f19ff8A5d8f3041f4';
 
 const counterContractAbi = [
   {
@@ -8,13 +8,24 @@ const counterContractAbi = [
     outputs: [],
     stateMutability: 'nonpayable',
   },
+  {
+    type: 'function',
+    name: 'number',
+    inputs: [],
+    outputs: [
+      {
+        name: '',
+        type: 'uint256',
+        internalType: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+  },
 ] as const;
 
 export const calls = [
   {
-    address: counterContractAddress,
-    abi: counterContractAbi,
-    functionName: 'increment',
-    args: [],
+    to: counterContractAddress as `0x${string}`,
+    data: '0xd09de08a', // increment() function selector
   },
 ];
